c0e5b3a32e2203b551491e527ad645ce29ea5b48
Thiago Luiz <thiagoluizgncalvesdefaria@gmail.com>
2025-05-06 18:50:01 -0300

    Criado o Projeto seguindo estrutura DDD

---
+++ MyRecipeBook.Application/MyRecipeBook.Application.cpp
@@ -0,0 +1,10 @@
+// MyRecipeBook.Application.cpp : Define as funções da biblioteca estática.
+//
+
+#include "pch.h"
+#include "framework.h"
+
+// TODO: Este é um exemplo de uma função de biblioteca
+void fnMyRecipeBookApplication()
+{
+}
